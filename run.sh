52fbcc32c352a7cdfa0670d49c1cff66410b36c8
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2019-04-03 23:28:51 -0300

    Add example with go-dnsperf

--- run.sh
+++ run.sh
@@ -3,5 +3,3 @@
 docker-compose up --abort-on-container-exit
 
 docker-compose -f docker-compose.hostnetwork.yml up --abort-on-container-exit
-
-docker-compose -f docker-compose.kubernetes.yml up --abort-on-container-exit
