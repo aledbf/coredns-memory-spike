f19bfa2e60b972c06d0882aa33088eea76b357d4
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2019-04-04 21:30:33 -0300

    Add multi node local cluster test

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Repository to reproduce issue https://github.com/coredns/coredns/issues/2593
 
 1. Clone the repo
 2. Run `./run.sh`
+
+3. Run `./run-in-kind.sh` to start a local cluster using [kind](https://github.com/kubernetes-sigs/kind)
+
+   Open `http://localhost:3000`
+
+   After the test run `kind delete cluster --name coredns` to destroy kind cluster
---
+++ run-in-kind.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+which helm || $(echo "Please install helm before running the script";exit 1)
+
+export KIND_CLUSTER_NAME="coredns"
+
+which kind || $(echo "Please install kind before running the script";exit 1)
+
+kind create cluster --name ${KIND_CLUSTER_NAME} --config ${DIR}/manifests/kind.yaml
+
+export KUBECONFIG="$(kind get kubeconfig-path --name="${KIND_CLUSTER_NAME}")"
+
+sleep 60
+
+which kubectl || $(echo "Please install kubectl before running the script";exit 1)
+
+kubectl config set-context kubernetes-admin@${KIND_CLUSTER_NAME}
+
+echo "Kubernetes cluster:"
+kubectl get nodes -o wide
+
+#kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+
+echo "Installing coredns"
+kubectl apply -f manifests/coredns.yaml
+
+echo "Installing Prometheus and Grafana"
+helm repo update
+
+helm init --upgrade
+
+sleep 60
+
+# https://github.com/fnproject/fn-helm/issues/21
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
+sleep 60
+
+helm install --name prometheus stable/prometheus \
+    --set persistentVolume.enabled=false \
+    --set alertmanager.persistentVolume.enabled=false \
+    --set pushgateway.persistentVolume.enabled=false \
+    --set server.persistentVolume.enabled=false \
+    --set deployCoreDNS=true
+
+helm install --name grafana -f manifests/grafana-values.yaml stable/grafana
+
+sleep 60
+
+echo "Installing go-dnsperf scaled to 30"
+kubectl apply -f manifests/deployment-ubuntu.yaml
+
+kubectl --namespace default port-forward svc/grafana 3000:80
